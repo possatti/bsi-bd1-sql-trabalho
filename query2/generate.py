b6c9c5c82fc0c40501b71b989066de63e7291490
possatti <lucas_possatti@hotmail.com>
2014-02-09 04:57:42 -0200

    Atualização para o script de geração da query2.sql.

--- query2/generate.py
+++ query2/generate.py
@@ -6,14 +6,20 @@ NOME_DO_ARQUIVO_GERADO = 'query2.sql'
 # Chama os scripts para gerarem seus arquivos.
 subprocess.call(['python3', 'Veiculo.py'])
 subprocess.call(['python3', 'VeiculoTipoCarga.py'])
+subprocess.call(['python3', 'Telefone.py'])
+subprocess.call(['python3', 'Endereco.py'])
+subprocess.call(['python3', 'Contato.py'])
 
 # Junta todos os SQLs em um único arquivo.
 # Atenção: A ordem importa! Devido as relações entre as tabelas,
 #          algumas devem ser preenchidas primeiro.
 SQLFiles = [
-'SQL Gerado/Veiculo.sql',
 'SQL Independente/TipoCarga.sql',
-'SQL Gerado/VeiculoTipoCarga.sql'
+'SQL Gerado/Veiculo.sql',
+'SQL Gerado/VeiculoTipoCarga.sql',
+'SQL Gerado/Contato.sql',
+'SQL Gerado/Telefone.sql',
+'SQL Gerado/Endereco.sql'
 ]
 
 with open(NOME_DO_ARQUIVO_GERADO, "w") as arqSaida:
