97aeb660697d3e4c0c05f8ccb4aaa32f1028be59
possatti <lucas_possatti@hotmail.com>
2014-02-09 03:00:14 -0200

    Foi criado um script para automatizar a construção da 'query2.sql'.
    Esse script deve ser modificado a medida que novos scripts ou queries
    SQL independentes forem criados.

--- query2/generate.py
+++ query2/generate.py
@@ -1,2 +1,23 @@
-import empresa
-import motorista
+import subprocess
+
+# Nome final do arquivo a ser gerado.
+NOME_DO_ARQUIVO_GERADO = 'query2.sql'
+
+# Chama os scripts para gerarem seus arquivos.
+subprocess.call(['python3', 'Veiculo.py'])
+subprocess.call(['python3', 'VeiculoTipoCarga.py'])
+
+# Junta todos os SQLs em um único arquivo.
+# Atenção: A ordem importa! Devido as relações entre as tabelas,
+#          algumas devem ser preenchidas primeiro.
+SQLFiles = [
+'SQL Gerado/Veiculo.sql',
+'SQL Independente/TipoCarga.sql',
+'SQL Gerado/VeiculoTipoCarga.sql'
+]
+
+with open(NOME_DO_ARQUIVO_GERADO, "w") as arqSaida:
+	for SQLFile in SQLFiles:
+		with open(SQLFile, "r") as arqEntrada:
+			for line in arqEntrada.readlines():
+				arqSaida.write(line)
